854c8105f9dff93b21afca5d0558411e1a85eb96
Aleksandra Borowska <aleks.borowska91@gmail.com>
2019-10-27 18:10:42 +0100

    initial version of working searchbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
@@ -24,6 +25,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot([
       {
         path: '',
--- src/app/planets-list/planets-list.component.ts
+++ src/app/planets-list/planets-list.component.ts
@@ -1,33 +1,75 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
 
-import { PlanetsService } from '../planets.service';
-import { Planet } from '../planet.model';
+import { PlanetsService } from "../planets.service";
+import { Planet } from "../planet.model";
 
 @Component({
-  selector: 'app-planets-list',
-  templateUrl: './planets-list.component.html',
-  styleUrls: ['./planets-list.component.scss']
+  selector: "app-planets-list",
+  templateUrl: "./planets-list.component.html",
+  styleUrls: ["./planets-list.component.scss"]
 })
 export class PlanetsListComponent implements OnInit, OnDestroy {
-
   arePlanetsLoading: boolean = false;
   subscriptionPlanets: Subscription;
-  planets: Planet[];
+  planetList: Planet[];
+  filteredPlanetList: Planet[];
+  // planetList: any;
 
-  constructor(private planetsService: PlanetsService) { }
 
-  ngOnInit() {
+  constructor(private planetsService: PlanetsService) {}
+
+  private _searchTerm: string;
+  get searchTerm(): string {
+    return this._searchTerm;
+  }
+  set searchTerm(value: string) {
+    this._searchTerm = value;
+    this.filteredPlanetList = this.filterList(value);
+  }
+
+  loadPlanetsData() {
     this.arePlanetsLoading = true;
 
     this.subscriptionPlanets = this.planetsService.getPlanets().subscribe(
       (planets: Planet[]) => {
         this.arePlanetsLoading = false;
-        this.planets = planets;
-    }, (error: Response) => {
-      alert('an unexpected error occured :-(');
-      console.log(error);
-    });
+        this.planetList = planets;
+        this.filteredPlanetList = this.planetList;
+      },
+      (error: Response) => {
+        alert("an unexpected error occured :-(");
+        console.log(error);
+      }
+    );
+  }
+
+
+  ngOnInit() {
+    // this.arePlanetsLoading = true;
+
+    // this.subscriptionPlanets = this.planetsService.getPlanets().subscribe(
+    //   (planets: Planet[]) => {
+    //     this.arePlanetsLoading = false;
+    //     this.planets = planets;
+    //   },
+    //   (error: Response) => {
+    //     alert("an unexpected error occured :-(");
+    //     console.log(error);
+    //   }
+    // );
+
+    this.loadPlanetsData();
+  }
+
+
+
+  filterList(value: string) {
+    console.log(value);
+
+    return this.planetList.filter(planet =>
+      planet.name.toLowerCase().indexOf(value.toLowerCase()) !== -1
+    );
   }
 
   ngOnDestroy() {
